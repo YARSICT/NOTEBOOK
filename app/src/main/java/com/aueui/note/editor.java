96ce6741d7c03652ebfb3c7bb89e7233ab39239f
aueui <yarsict@126.com>
2018-10-29 20:51:15 +0800

    1.0.0-a19-R1

--- app/src/main/java/com/aueui/note/editor.java
+++ app/src/main/java/com/aueui/note/editor.java
@@ -16,10 +16,13 @@ Copyright 2018 YARSICT
 package com.aueui.note;
 
 import android.app.ActionBar;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
@@ -32,6 +35,13 @@ import com.aueui.note.write.notes;
 import org.litepal.LitePal;
 import org.litepal.crud.LitePalSupport;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -56,16 +66,27 @@ public class editor extends BaseActivity {
         Button note_commit = (Button) findViewById(R.id.note_commit);
         editText = (EditText) findViewById(R.id.note_context);
         editText1 = (EditText) findViewById(R.id.note_title);
-        if (editText.getText().toString().equals("")) {
-
+        Log.i("load", load());
+        SharedPreferences sharedPreferences = getSharedPreferences("com.aueui.note_preferences", MODE_PRIVATE);
+        String where = sharedPreferences.getString("where", "");
+        if (where.equals("read")) {
+            Intent intent = getIntent();
+            title = intent.getStringExtra("title");
+            context = intent.getStringExtra("context");
+            //  where=intent.getStringExtra("where");
+            editText.setText(context);
+            editText1.setText(title);
         }
-        //  if (isread==true) {
-        Intent intent = getIntent();
-        title = intent.getStringExtra("title");
-        context = intent.getStringExtra("context");
-        //  where=intent.getStringExtra("where");
-        editText.setText(context);
-        editText1.setText(title);
+        if (where.equals("MainActivity")) {
+            if (!TextUtils.isEmpty(load())) {
+                editText.setText(load());
+                editText.setSelection(load().length());
+            }
+            if (!TextUtils.isEmpty(load_title())) {
+                editText1.setText(load_title());
+                editText1.setSelection(load_title().length());
+            }
+        }
 
         // }
         note_commit.setOnClickListener(new View.OnClickListener() {
@@ -74,12 +95,10 @@ public class editor extends BaseActivity {
                 EditText note_context = (EditText) findViewById(R.id.note_context);
                 EditText note_title = (EditText) findViewById(R.id.note_title);
                 Date date = new Date(System.currentTimeMillis());
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");// HH:mm:ss
-
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
                 String date_string = simpleDateFormat.format(date);
                 //Toast.makeText(editor.this,date_string,Toast.LENGTH_LONG).show();
                 notes notes = new notes();
-
                 note_text = editText.getText().toString();
                 note_titles = note_title.getText().toString();
                 SharedPreferences sharedPreferences = getSharedPreferences("com.aueui.note_preferences", MODE_PRIVATE);
@@ -102,6 +121,49 @@ public class editor extends BaseActivity {
                     notes.setDate(date_string);
                     notes.save();
                 }
+                SharedPreferences.Editor editor = getSharedPreferences("com.aueui.note_preferences", MODE_PRIVATE).edit();
+                editor.putString("isCommit", "commit");
+                editor.apply();
+                FileOutputStream out = null;
+                BufferedWriter writer = null;
+                try {
+                    out = openFileOutput("cache_data", Context.MODE_PRIVATE);
+                    writer = new BufferedWriter(new OutputStreamWriter(out));
+                    writer.write("");
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        {
+                            if (writer != null) {
+                                writer.close();
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                try {
+                    out = openFileOutput("title_data", Context.MODE_PRIVATE);
+                    writer = new BufferedWriter(new OutputStreamWriter(out));
+                    writer.write("");
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        {
+                            if (writer != null) {
+                                writer.close();
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
                 Intent intent = new Intent(editor.this, MainActivity.class);
                 startActivity(intent);
                 finish();
@@ -110,6 +172,125 @@ public class editor extends BaseActivity {
             }
         });
 
+    }
+
+    public void save(String input) {
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+        try {
+            out = openFileOutput("cache_data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(input);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                {
+                    if (writer != null) {
+                        writer.close();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public void save_title(String titles) {
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+        try {
+            out = openFileOutput("title_data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(titles);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                {
+                    if (writer != null) {
+                        writer.close();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+    protected void onDestroy() {
+        super.onDestroy();
+        String input = editText.getText().toString();
+        String titles=editText1.getText().toString();
+        SharedPreferences sharedPreferences = getSharedPreferences("com.aueui.note_preferences", MODE_PRIVATE);
+        String isCommit = sharedPreferences.getString("isCommit", "");
+        if (isCommit.equals("commit")) {
+            SharedPreferences.Editor editor = getSharedPreferences("com.aueui.note_preferences", MODE_PRIVATE).edit();
+            editor.putString("isCommit", "uncommit");
+            editor.apply();
+        } else {
+            save(input);
+            save_title(titles);
+        }
+    }
+
+    public String load() {
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            in = openFileInput("cache_data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return content.toString();
+            }
+        }
+    }
+
+    public String load_title() {
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            in = openFileInput("title_data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return content.toString();
+            }
+        }
     }
 
     @Override
